7632d9631797ee1df7848f30c6f950f1e2e1f64b
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-01 14:45:02 -0300

    new: findAll User

--- src/database/repository/UserRepository.ts
+++ src/database/repository/UserRepository.ts
@@ -4,6 +4,10 @@ import { User } from '../entities/User';
 export class UserRepository {
   constructor(private readonly userRepository: Repository<User>) {}
 
+  async findAll(): Promise<User[]> {
+    return this.userRepository.query(`select * from user`);
+  }
+
   async save(user: User): Promise<User> {
     return this.userRepository.save(user);
   }
