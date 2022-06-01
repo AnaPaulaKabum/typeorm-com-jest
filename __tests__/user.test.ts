bcfadfad1874e80c1f4ce0409fbdda8584dd6dfa
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-01 14:12:27 -0300

    test: save user

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -1,19 +1,24 @@
 import connection from '../src/connection';
 import { User } from '../src/database/entities/User';
+import { UserRepository } from '../src/database/repository/UserRepository';
 
 describe('users', () => {
+  let userRepository: UserRepository;
+
   //Executa uma função antes de qualquer um dos testes
   beforeAll(async () => {
+    const tableName = 'user';
     await connection.create();
-    await connection.clear('user');
+    await connection.clear(tableName);
+    userRepository = new UserRepository(connection.getRepository(tableName));
   });
-  //Executa uma função depois de qualquer um dos testes
 
   //Limpar os registros depois de cada teste.
   afterEach(async () => {
     //await connection.clear('user');
   });
 
+  //Executa uma função depois de qualquer um dos testes
   afterAll(async () => {
     await connection.close();
   });
@@ -22,7 +27,7 @@ describe('users', () => {
     user.age = 19;
     user.firstName = 'ana';
     user.lastName = 'paula';
-    await connection.createUser(user);
-    expect(1).toBe(1);
+    const userSave = await userRepository.save(user);
+    expect(userSave.id).toBeTruthy();
   });
 });
