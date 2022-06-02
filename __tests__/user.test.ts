12a4b2ac6d191273cc7cbf6e7a5591baf32cd1ce
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-06-02 11:24:15 -0300

    test: create delete and findall with 1 regiter

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -31,11 +31,6 @@ describe('users', () => {
     expect(userSave.id).toBeTruthy();
   });
 
-  it('findAll user', async () => {
-    const listUser = await userRepository.findAll();
-    expect(listUser.length).toBe(0);
-  });
-
   it('update user', async () => {
     const userUpdate = new User();
     userUpdate.age = 19;
@@ -51,5 +46,33 @@ describe('users', () => {
     expect(resultUpdate?.age).toBe(userUpdate.age);
     expect(resultUpdate?.lastName).toBe(userUpdate.lastName);
     expect(resultUpdate?.firstName).toBe(userUpdate.firstName);
+  });
+
+  it('findAll user', async () => {
+    const listUser = await userRepository.findAll();
+    expect(listUser.length).toBe(0);
+  });
+
+  it('findAll with 1 register', async () => {
+    const userUpdate = new User();
+    userUpdate.age = 19;
+    userUpdate.firstName = 'ana';
+    userUpdate.lastName = 'fernandes';
+    await userRepository.save(userUpdate);
+
+    let listUser = await userRepository.findAll();
+    expect(listUser.length).toBe(1);
+  });
+
+  it('delete user', async () => {
+    const userUpdate = new User();
+    userUpdate.age = 19;
+    userUpdate.firstName = 'ana';
+    userUpdate.lastName = 'fernandes';
+    const userSave = await userRepository.save(userUpdate);
+
+    await userRepository.delete(userSave.id);
+    const listUser = await userRepository.findAll();
+    expect(listUser.length).toBe(0);
   });
 });
